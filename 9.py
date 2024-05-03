373ebeff8b89fb979b24894db696f1e31cfb08a9
ShiJiliana <ulgulakova@yandex.ru>
2024-05-04 00:45:59 +0700

    add

---
+++ 9.py
@@ -0,0 +1,16 @@
+import os
+
+if not os.path.isdir('simple'):
+    os.mkdir('simple')
+
+with open('input9.txt', 'r') as f:
+    lines = f.readlines()
+    h = []
+    h1 = []
+    for line in lines:
+        h.append(line.strip())
+    h1 = h[1::2]
+    with open('output9.txt', 'w') as f1:
+       print('\n'.join(h1), file=f1)
+
+os.replace('output9.txt', 'simple/output9.txt')
